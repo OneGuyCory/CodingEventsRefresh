d0f474555e25e5c61eb6a1e34a7f9f7b491bad50
Cory <corydeuschle92@gmail.com>
2023-07-24 21:36:46 -0500

    Add project files.

---
+++ CodingEventsRefresh/Controllers/EventsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEventsRefresh.Controllers
+{
+    public class EventsController : Controller
+    {
+
+       private static List<string> Events = new List<string>();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.events = Events;
+            
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Add")]
+        public IActionResult NewEvent(string name)
+        {
+            Events.Add(name);
+
+            return Redirect("/Events");
+        }
+    }
+}
